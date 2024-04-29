66a717fef5fa51e9841b69b8294ce571475a788c
sungminiioo <162776846+sungminiioo@users.noreply.github.com>
2024-04-29 06:32:39 +0000

    commit

--- bot.py
+++ bot.py
@@ -1,17 +1,17 @@
 import random
 
 # This list contains the random responses (you can add your own or translate them into your own language too)
-random_responses = ["That is quite interesting, please tell me more.",
-                    "I see. Do go on.",
-                    "Why do you say that?",
-                    "Funny weather we've been having, isn't it?",
-                    "Let's change the subject.",
-                    "Did you catch the game last night?"]
+random_responses = ["그거 참 흥미로운데요, 더 자세히 말해주세요.",
+                    "알겠어요. 계속 이야기해주세요.",
+                    "왜 그렇게 말하시는 건가요?",
+                    "요즘 날씨가 웃긴데, 그렇죠?",
+                    "주제를 바꿔볼까요?",
+                    "어젯밤 경기 보셨나요?"]
 
-print("Hello, I am Marvin, the simple robot.")
-print("You can end this conversation at any time by typing 'bye'")
-print("After typing each answer, press 'enter'")
-print("How are you today?")
+print("안녕하세요, 저는 Marvin이라고 하는 간단한 로봇입니다.")
+print("'bye'를 입력하여 언제든 대화를 마칠 수 있습니다.")
+print("각 응답을 입력한 후 '엔터' 키를 누르세요.")
+print("오늘 기분은 어떠신가요?")
 
 while True:
     # wait for the user to enter some text
@@ -23,4 +23,4 @@ while True:
         response = random.choices(random_responses)[0]
     print(response)
 
-print("It was nice talking to you, goodbye!")
+print("대화가 좋았어요. 안녕히 계세요!")
